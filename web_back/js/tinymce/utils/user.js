32f762667b2533b25fef9995da529631b199f5ff
jiangyuhang19971225 <1136040732@qq.com>
2019-09-21 17:49:33 +0800

    后台退出功能

--- web_back/js/tinymce/utils/user.js
+++ web_back/js/tinymce/utils/user.js
@@ -6,15 +6,13 @@ var user = {
      * @param {*} call 回调函数
      */
     login: function (username, password, call) {
-        $.post('http://localhost:8000/admin/login', {
-            user_name: username,
-            password: password
-        }, function (res) {
+        $.post('http://localhost:8000/admin/login', { user_name: username, password: password }, function (res) {
             call(res);
-
-
-
-
+        })
+    },
+    logout: function (call) {
+        $.post('http://localhost:8000/admin/logout', function (res) {
+            call(res);
         })
     }
 }
